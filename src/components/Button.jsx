472e3be19a04b16cab3986fe789d58eda60f4eaf
Bakhtiyar Zubairov <irookanji@gmail.com>
2022-05-23 23:25:39 +0200

    Add Homepage

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { useStateContext } from "../contexts/ContextProvider";
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ bgColor, color, bgHoverColor, size, text, borderRadius }) => {
+  const { setIsClicked, initialState } = useStateContext();
+
+  return (
+    <button
+      type="button"
+      style={{ backgroundColor: bgColor, color, borderRadius }}
+      className={` text-${size} p-3 hover:drop-shadow-xl hover:bg-${bgHoverColor}`}
+    >
+      {text}
+    </button>
+  );
 };
 
 export default Button;
