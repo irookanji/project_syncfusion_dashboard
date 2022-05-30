5cee8b0c8b10cbbc4ff714f065182b912b36db08
Bakhtiyar Zubairov <irookanji@gmail.com>
2022-05-30 23:14:34 +0200

    Add ThemeSettings component and add some fixes in Context provider

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ import { useStateContext } from "./contexts/ContextProvider";
 import "./App.css";
 
 const App = () => {
-  const { activeMenu } = useStateContext();
+  const { activeMenu, themeSettings, setThemeSettings } = useStateContext();
 
   return (
     <div>
@@ -39,6 +39,7 @@ const App = () => {
               <button
                 type="button"
                 className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+                onClick={() => setThemeSettings(true)}
                 style={{ background: "blue", borderRadius: "50%" }}
               >
                 <FiSettings />
@@ -64,6 +65,8 @@ const App = () => {
             </div>
 
             <div>
+              {themeSettings && <ThemeSettings />}
+
               <Routes>
                 {/* Dashboard  */}
                 <Route path="/" element={<Ecommerce />} />
